67009fa69aa44bad984d8cbf06dbad5b12a2cb6a
zhushh <zsh5318@gmail.com>
2014-05-28 01:37:30 +0800

    Create usage_of_for-2.sh

---
+++ usage_of_for-2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Program:
+#	User input dir name, I find the permission of files.
+# From:
+# 	鸟哥的linux私房菜（基础篇）
+# History:
+# 2014/05/27	zhushuhuang	First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+read -p "Please input a directory: " dir
+if [ "$dir" == "" -o ! -d "$dir" ]; then
+	echo "The $dir is NOT exist in your system."
+	exit 1
+fi
+
+filelist=$(ls $dir)
+for filename in $filelist
+do
+	perm=""
+	test -r "$dir/$filename" && perm="$perm readable"
+	test -w "$dir/$filename" && perm="$perm writable"
+	test -x "$dir/$filename" && perm="$perm executable"
+	echo "The file $dir/$filename'spermission is $perm "
+done
