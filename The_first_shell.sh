c233776cfadbe0d77094908d8c37e58f8f4ce7bb
zhushh <zsh5318@gmail.com>
2014-05-28 01:00:02 +0800

    Create The_first_shell.sh

---
+++ The_first_shell.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Program:
+#   This program show 'Hello world' in your screen.
+# From:
+#   鸟哥的Linux私房菜（基础篇）
+# History:
+# 2014/05/27    zhushuhuang zsh5318@gmail.com   First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+echo -e "Hello world! \a \n"
+exit 0
