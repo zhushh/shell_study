04ba80d67a233dc0f98a8085fd9932d908d22685
朱术煌 <zsh5318@gmail.com>
2015-06-23 02:57:55 +0800

    Update shift.sh

--- shift.sh
+++ shift.sh
@@ -6,9 +6,10 @@ count=0   # initailize variable count
 number=$#    # Total number of command-line arguments to process
 
 # start a while loop
-while [ $count -lt $number ] do
+while [ $count -lt $number ] 
+do
     count=`expr $count + 1`  # increase 1 to variable count
     token='$'$count           # loops through each token starting with $1
-
+    echo $count     # output the count
     shift        # Grab the next token, i.e. $2 become $1
 done
